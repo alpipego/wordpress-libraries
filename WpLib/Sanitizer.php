9c2a5d840601c09dfaf438708f161e8b14ecbf44
alpipego <alpipego@gmail.com>
2016-08-17 15:03:38 +0200

    change strtolower to mb_stringtolower

--- WpLib/Sanitizer.php
+++ WpLib/Sanitizer.php
@@ -6,11 +6,11 @@ class Sanitizer
 {
     public function name($string)
     {
-        return str_replace(['ä', 'ü', 'ö', 'ß'], ['ae', 'ue', 'oe', 'ss'], preg_replace('%\h%', '-', strtolower((string) $string)));
+        return str_replace(['ä', 'ü', 'ö', 'ß'], ['ae', 'ue', 'oe', 'ss'], preg_replace('%\h%', '-', mb_strtolower((string) $string)));
     }
 
     public function permalink($string)
     {
-        return str_replace(['ä', 'ü', 'ö', 'ß', ' '], ['ae', 'ue', 'oe', 'ss', '%20'], strtolower((string) $string));
+        return str_replace(['ä', 'ü', 'ö', 'ß', ' '], ['ae', 'ue', 'oe', 'ss', '%20'], mb_strtolower((string) $string));
     }
 }
