10e43e3dac02146f7334ae990908fb65c4afd1d7
alpipego <alpipego@gmail.com>
2016-10-18 09:54:39 +0200

    code style

--- WpLib/Helpers.php
+++ WpLib/Helpers.php
@@ -2,20 +2,17 @@
 
 namespace Alpipego\WpLib;
 
-class Helpers
-{
+class Helpers {
 
-    public static function notEmpty($var)
-    {
-        return (isset($var) && !empty($var));
-    }
+	public static function notEmpty( $var ) {
+		return ( isset( $var ) && ! empty( $var ) );
+	}
 
-    public static function log($log, $exit = false)
-    {
-        $trace = debug_backtrace();
-        error_log(date('H:i:s', strtotime('now')) . ' ' . $trace[0]['file'] . ':' . $trace[0]['line'] . "\n" . print_r($log, true) . "\n", 3, WP_CONTENT_DIR . '/debug.log');
-        if ($exit) {
-            wp_die();
-        }
-    }
+	public static function log( $log, $exit = false ) {
+		$trace = debug_backtrace();
+		error_log( date( 'H:i:s', strtotime( 'now' ) ) . ' ' . $trace[0]['file'] . ':' . $trace[0]['line'] . "\n" . print_r( $log, true ) . "\n" );
+		if ( $exit ) {
+			wp_die();
+		}
+	}
 }
