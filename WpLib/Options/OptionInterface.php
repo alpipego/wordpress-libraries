26ed6b9284723d30fc7b490e26ed8fdb068cfcfc
alpipego <alpipego@gmail.com>
2016-12-08 10:51:38 +0100

    WIP

---
+++ WpLib/Options/OptionInterface.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 26/07/16
+ * Time: 12:18
+ */
+
+namespace Alpipego\WpLib\Options;
+
+
+/**
+ * Interface OptionInterface
+ * @package Alpipego\WpLib
+ */
+interface OptionInterface {
+	/**
+	 * @return mixed
+	 */
+	public function run();
+
+	/**
+	 * @return mixed
+	 */
+	public function registerSetting();
+
+	/**
+	 * @return mixed
+	 */
+	public function addField();
+
+	/**
+	 * @return mixed
+	 */
+	public function callback();
+
+	/**
+	 * @param $value
+	 *
+	 * @return mixed
+	 */
+	public function sanitize( $value );
+}
---
+++ WpLib/Plugin/Boilerplate.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alpipego
+ * Date: 17/10/2016
+ * Time: 18:39
+ */
+
+namespace Alpipego\WpLib\Plugin;
+
+
+class Boilerplate {
+	protected $file;
+	protected $path;
+	private $textDomain;
+	private $languagesDir;
+
+	public function __construct( $file, $activation = null, $textdomain = '', $languagesDir = __DIR__ . 'languages', $deactivation = null) {
+		// parent variable
+		$this->file         = $file;
+		$this->textDomain   = $textdomain;
+		$this->languagesDir = $languagesDir;
+
+		// check if acf plugin is active and pass custom (static) activation function on activation
+		$this->activationHook( $activation );
+		$this->deactivationHook( $deactivation );
+	}
+
+	protected function activationHook( $func ) {
+		\register_activation_hook( $this->file, function () use ( $func ) {
+			$this->checkDependencies();
+			if ( ! is_null( $func ) ) {
+				call_user_func( $func );
+			}
+		} );
+	}
+
+	protected function checkDependencies() {
+	}
+
+	protected function deactivationHook( $func ) {
+		\register_deactivation_hook( $this->file, function () use ( $func ) {
+			if ( ! is_null( $func ) ) {
+				call_user_func( $func );
+			}
+		} );
+	}
+
+	public function run() {
+		$this->checkDependencies();
+		add_action( 'plugins_loaded', [ $this, 'loadTextdomain' ] );
+	}
+
+	public function getPath() {
+		$path = explode( '/', $this->path );
+
+		return \trailingslashit( WP_PLUGIN_DIR ) . $path[0];
+	}
+
+	/**
+	 * wrapper for `load_plugin_textdomain`
+	 *
+	 * @param string $dir path to languages dir
+	 */
+	public function loadTextdomain() {
+		if ( ! empty( $this->textDomain ) ) {
+			\load_plugin_textdomain( $this->textDomain, false, $this->languagesDir );
+		}
+	}
+}
